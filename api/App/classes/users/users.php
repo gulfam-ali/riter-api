01310250e23e9bcfbc0bbc6a49a16772d6b79edc
Gulfam Ali <aligulfam6@gmail.com>
2018-02-08 00:23:36 +0530

    All register, login, logout, password reset code and reset added to one class and controller userController. Looking forward to delete unwanted files related to this.

--- api/App/classes/users/users.php
+++ api/App/classes/users/users.php
@@ -1,64 +1,216 @@
 <?php
 class User extends Builder
 {
-
 	public $config;
+	public $mail;
 
     public function __construct($db) {
-      $this->db = $db;
+		$this->db = $db;
 
-      global $config;
-      $this->config = (object) $config;
+		global $config;
+		$this->config = (object) $config;
+
+		require_once((CLASS_PATH . '/send_mail.php'));
+		$this->mail = new Mail();
   	}
-
-	public function save_password()
-	{
+	
+	public function login()
+    {
 		$data = json_decode(file_get_contents("php://input"));
-        $currentPassword = $this->valid_input($data->currentPassword);
-		$newPassword = $this->encryptIt( $this->valid_input($data->newPassword) );
+		
+        $email = $this->valid_input($data->email);
+        $password = $data->password;
+		
+		$check_exist = $this->select( "users", " email='".$email."' " );
 
-		$user_id = $_COOKIE['userid'];
-
-		$result = $this->select("users", " id = ".$user_id);
-		if($result['validate']=='true')
-		{
-			if($result['total_records']>0)
-			{
-				$checkPass = $this->decryptIt($result['data'][0]['password']);
-
-				if($checkPass != $currentPassword)
-				{
-					$response['validate'] = 'false';
-					$response['message'] = 'Incorrect Current Password';
-					return $response;
+		if($check_exist['validate']==='empty'){
+			
+			$response['validate'] = 'false';
+			$response['message'] = "This email is not registered";
+			return $response;
+		}
+		else{
+			if(password_verify($password, $check_exist['data'][0]['password'])) {
+				$user_id = $check_exist['data'][0]['id'];
+				$first_name = $check_exist['data'][0]['first_name'];
+				$last_name = $check_exist['data'][0]['last_name'];
+				$avtar = $check_exist['data'][0]['avtar'];
+				
+				$user_token = $this->getToken(32);
+				
+				$insert_values = " user_id = '".$user_id."', token='".$user_token."' ";
+				$flag = $this->insert("sessions", $insert_values);
+				
+				if($flag){
+					$response['user_id'] = $user_id;
+					$response['first_name'] = $first_name;
+					$response['last_name'] = $last_name;
+					$response['avtar'] = $avtar;
+					
+					$response['user_token'] = $user_token;
+					$response['validate'] = 'true';
+					$response['message'] = 'Login Success';
 				}else{
-					$flag = $this->insert("users", " password = '".$newPassword."' ", " id = ".$user_id);
-					if($flag)
-					{
-						$response['validate'] = 'true';
-						$response['message'] = 'Password changed successfully';
-					}else{
-						$response['validate'] = 'false';
-						$response['message'] = 'Password cannot be changed. Please try again later';
-					}
+					$response['validate'] = 'false';
+					$response['message'] = 'Server is not responding at the moment. Please try again later.';
+				}
+				
+			} 
+			else {
+				$response['validate'] = 'false';
+				$response['message'] = 'Incorrect Password.';
+				
+			}
+			
+			
+			
+			return $response;
+		}
+    }
+	
+	public function logout()
+    {
+		$data = json_decode(file_get_contents("php://input"));
+		
+        $user_id = $this->valid_input($data->user_id);
+		$token = $this->valid_input($data->token);
+		
+		$flag = $this->update("sessions", " is_logout = 1", " user_id=".$user_id." AND token = '".$token."'" );
+		$response['validate'] = 'true';
+		return $response;
+    }
+	
+	public function register()
+    {
+		$data = json_decode(file_get_contents("php://input"));
+		
+        $first_name = $this->valid_input($data->firstName);
+        $last_name = $this->valid_input($data->lastName);
+        $email = $this->valid_input($data->email);
+        $password = $data->password;
+        $cpassword = $data->confirmPassword;
+		
+		if($password !== $cpassword)
+		{
+			$response['validate'] = 'password';
+			$response['message'] = "Password does not match";
+			return $response;
+		}
+		$check_exist = $this->select("users", " email='".$email."' ");
 
-					return $response;
+		if($check_exist['validate']==='empty'){
+			
+			$options = [
+				'cost' => 12,
+			];
+			$password = password_hash($password, PASSWORD_BCRYPT, $options);
+			
+			$insert_values = " first_name = '".$first_name."', last_name='".$last_name."', email='".$email."', password='".$password."' ";
+			$flag = $this->insert("users", $insert_values);
+			
+			if($flag)
+			{
+				$response['validate'] = 'true';
+				$response['message'] = 'Registration Success';
+			}else{
+				$response['validate'] = 'false';
+				$response['message'] = 'Registration Failed';
+			}
+			
+			return $response;
+		}
+		else{
+			$response['validate'] = 'exist';
+			$response['message'] = "This email is already registered.";
+			return $response;
+		}
+    }
+	
+	public function reset_code()
+    {
+		$data = json_decode(file_get_contents("php://input"));
+        $email = $this->valid_input($data->email);
+		
+		
+		$check_exist = $this->select( "users", " email='".$email."' " );
+		if($check_exist['validate']==='empty'){
+			
+			$response['validate'] = 'false';
+			$response['message'] = "This email is not registered";
+		}
+		else{
+			$user_id = $check_exist['data'][0]['id'];
+			$first_name = $check_exist['data'][0]['first_name'];
+			$last_name = $check_exist['data'][0]['last_name'];
+			$full_name = $first_name.' '.$last_name;
+			$code = mt_rand(21212, 98989);
+			
+			$msg = '
+			<p><b>Hi '.$first_name.',</b></p>'
+			.'<p>To change your account password, enter the following reset code in the box provided in your browser:</p>'
+			.'<p style="font-size: 2em; color: #888; font-weight: bold;">'.$code.'</p>'
+			.'<p>This reset code will expire in 1 hour, so be sure to use it right away.</p>';
+	
+			$status = $this->mail->send_mail($email, $full_name, 'Password Reset Code', $msg);
+			
+			if($status){
+				$insert_values = " user_id = '".$user_id."', reset_code='".$code."' ";
+				$flag = $this->insert("password_reset", $insert_values);
+				
+				if($flag){
+					$response['validate'] = 'true';
+					$response['message'] = " Password reset code successfully sent to your email address.";
+				}else{
+					$response['validate'] = 'false';
+					$response['message'] = " Something went wrong. Please try again later.";
 				}
 			}
+			else{
+				$response['validate'] = 'false';
+				$response['message'] = " Something went wrong. Please try again later.";
+			}
 		}
-	
-	}
-	
-	function encryptIt( $q ) {
-		$cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
-		$qEncoded      = base64_encode( mcrypt_encrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), $q, MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ) );
-		return( $qEncoded );
-	}
+		
+		return $response;
+    }
 
-	function decryptIt( $q ) {
-		$cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
-		$qDecoded      = rtrim( mcrypt_decrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), base64_decode( $q ), MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ), "\0");
-		return( $qDecoded );
+	public function reset_password()
+	{
+		$data = json_decode(file_get_contents("php://input"));
+		
+		$email = $this->valid_input($data->email);
+		$code = $this->valid_input($data->code);
+		$password = $this->valid_input($data->password);
+		
+		$sql= 	" SELECT U.id FROM pr_users U "
+				." JOIN pr_password_reset PR ON U.id=PR.user_id "
+				." WHERE U.email='".$email."' AND PR.reset_code='".$code."' ";
+
+		$check_exist = $this->custom($sql);
+		if($check_exist->num_rows>0){
+			$userData = mysqli_fetch_object($check_exist);
+			$user_id = $userData->id;
+			
+			$options = ['cost' => 12];
+			$password = password_hash($password, PASSWORD_BCRYPT, $options);
+			
+			$flag = $this->update("users", " password = '".$password."' ", " id=".$user_id );
+			
+			if($flag){
+				$response['validate'] = "true";
+				$response['message'] = "Password reset successfully. Login to your account now with your new password.";
+			}else{
+				$response['validate'] = "false";
+				$response['message'] = "Unable to connect to server. Please try again later.";
+			}
+		}else{
+			$response['validate'] = "false";
+			$response['message'] = " Incorrect email or reset code. Please check your email for reset code.";
+		}
+		
+		
+		return $response;
+	
 	}
 
 }
---
+++ api/App/controllers/userController.php
@@ -0,0 +1,68 @@
+<?php
+require_once(CONTROLLER_PATH ."/controller.php");
+class userController extends controller{
+
+	public $userClass;
+	public function __construct($db) {
+		require_once((CLASS_PATH . '/users/users.php'));
+		$this->userClass = new User($db);
+		
+	}
+
+	public function call($db, $method){
+		
+		$requestUrl = array();
+		if(isset($_REQUEST['requestUrl'])){
+			$requestUrl = explode('/', $_REQUEST['requestUrl']);
+		}
+		
+		switch($method) {
+			case 'PUT':
+			  break;
+
+			case 'DELETE':
+			  break;
+
+			case 'GET':
+			break;
+
+			case 'POST':
+			if(isset($requestUrl[1])){
+				if(trim($requestUrl[1]) == 'login')
+				{
+					return $this->userClass->login();
+				}
+				else if(trim($requestUrl[1]) == 'logout')
+				{
+					return $this->userClass->logout();
+				}
+				else if(trim($requestUrl[1]) == 'register')
+				{
+					return $this->userClass->register();
+				}
+				else if(trim($requestUrl[1]) == 'reset-password')
+				{
+					return $this->userClass->reset_password();
+				}
+				else if(trim($requestUrl[1]) == 'reset-code')
+				{
+					return $this->userClass->reset_code();
+				}
+				else{
+					return 404;
+				}
+			}else{
+				return 0;
+			}
+			break;
+
+			default:
+			  header('HTTP/1.1 405 Method Not Allowed');
+			  header('Allow: GET, PUT, DELETE');
+			  break;
+		}
+	}
+
+
+}
+?>
